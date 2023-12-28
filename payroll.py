6f998ee77612acbf9def0e66691cbaea73eb37b4
hugosmeyer <hugosmeyer@gmail.com>
2023-12-28 13:17:43 +0200

    After adding filtering, totalling and bold fonts

--- payroll.py
+++ payroll.py
@@ -7,8 +7,6 @@ from openpyxl.styles import PatternFill, Border, Side, Alignment, Protection, Fo
 from openpyxl.utils import get_column_letter
 import os
 import sys
-import configparser
-
 
 # Check if a file exists
 def check_file(filename):
@@ -24,20 +22,32 @@ def defnfileprse(file_path):
     ini_data = dict()
 
     with open(file_path, 'r') as file:
-        current_section = None
+        currsect = None
         for line in file:
             line = line.strip()
             if line.startswith('[') and line.endswith(']'):
-                current_section = line[1:-1]
-                ini_data[current_section] = list()
+                currsect = line[1:-1]
+                ini_data[currsect] = list()
             elif '=' in line:
                 key, value = map(str.strip, line.split('=', 1))
                 if not key == "":
                     if key is None:
                        key = ""
-                    ini_data[current_section].append([key, value])
+                    ini_data[currsect].append([key, value])
     return ini_data
 
+def copycellfrmt(fromcell, destcell):
+    destcell.font          = copy(fromcell.font)
+    destcell.border        = copy(fromcell.border)
+    destcell.fill          = copy(fromcell.fill)
+    destcell.number_format = copy(fromcell.number_format)
+    destcell.protection    = copy(fromcell.protection)
+    destcell.alignment     = copy(fromcell.alignment)
+
+def makefontbold(cell):
+    thisfont = copy(cell.font)
+    thisfont.bold = True
+    cell.font = thisfont
 
 # Main, start of the program
 if __name__ == "__main__":
@@ -45,14 +55,18 @@ if __name__ == "__main__":
     #Assume all files are present
     filepass = True
 
+    busnunitname = str(sys.argv[1])
+    exclfilename = str(sys.argv[2])
+    cldrmnth     = str(sys.argv[3])
+    cldryear     = str(sys.argv[4])
+    defnfilename = busnunitname + ".ini"
+
     # Check if definition file exists
-    defnfilename = sys.argv[1]
     filechckbool, filechckrslt = check_file(defnfilename)
     if not filechckbool:
         filepass = False
     
     # Check if Excel file exists
-    exclfilename = sys.argv[2]
     filechckbool, filechckrslt = check_file(exclfilename)
     if not filechckbool:
         filepass = False
@@ -68,9 +82,7 @@ exclmainbook = load_workbook(exclfilename,data_only=True)
 exclmainname = exclmainbook.sheetnames[0]
 exclmainshet = exclmainbook[exclmainbook.sheetnames[0]]
 exclrowsused = exclmainshet.max_row
-#print("max_row = ",exclrowsused)
 exclcolsused = exclmainshet.max_column
-#print("max_col = ",exclcolsused)
 
 colmcntr = 1
 maincolmhdrs = {}
@@ -79,7 +91,29 @@ while colmcntr <= exclcolsused:
     colmcntr += 1
 
 for defn in defndict:
+    thisdefn = defndict[defn]
+    
+    # Default this in case there is no SKIP
+    rowsstrt = 1
+
+    repldefn = list()
+    for testname, testvalu in thisdefn:
+        testskip = False
+        if testname == "SKIP":
+            testvalu.strip()
+            if testvalu.isdigit():
+                rowsstrt = int(testvalu)
+                rowsstrt += 1   
+            testskip = True
+    
+        if not testskip:
+            repldefn.append([testname, testvalu])
+
+    thisdefn = repldefn
+    del repldefn
+    
     defnname, defntype = defn.split(".")
+    
     if defntype == "FILE":
         destbook = Workbook()
         destshet = destbook.active
@@ -90,12 +124,28 @@ for defn in defndict:
     chdrfont = Font(name='Arial',size=10,bold=True,italic=False,vertAlign=None,underline='none',strike=False,color='FF000000')
     chdrfill = PatternFill(fill_type="lightGray",start_color='FF555555',end_color='FF555555')
     chdralgn = alignment=Alignment(horizontal='general',vertical='center',text_rotation=0,wrap_text=True,shrink_to_fit=False,indent=0)
+
     destcolm = 1
-    for defncolm in defndict[defn]:
+    nzrocols = []
+    totlcols = []
+    totlflag = False
+
+    colmcntr = 1
+    for defncolm in thisdefn:
         maincolm,thiscolm = defncolm
+        if "_NZ_" in thiscolm:
+            nzrocols.append(colmcntr)
+            thiscolm.replace("_NZ_", "")
         
+        if "_SUM_" in thiscolm:
+            totlcols.append(colmcntr)
+            totlflag = True
+            thiscolm.replace("_SUM_", "")
+            
+        colmcntr += 1
+
         # insert the header
-        destcell = destshet.cell(row = 1, column = destcolm)
+        destcell = destshet.cell(row = rowsstrt, column = destcolm)
         destshet.row_dimensions[1].height = None
         if maincolm in maincolmhdrs:
             fromcell=exclmainshet.cell(row = 1, column = maincolmhdrs[maincolm])
@@ -109,37 +159,78 @@ for defn in defndict:
         # load the values below the header
         rowscntr = 2
         while rowscntr <= exclrowsused:
-            #print("maincolm = ",maincolm)
-            #print("rowscntr = ",rowscntr)
-            #print("destcolm = ",destcolm)
-            destcell = destshet.cell(row = rowscntr, column = destcolm)
+            destcell = destshet.cell(row = rowscntr + rowsstrt - 1, column = destcolm)
             destshet.row_dimensions[rowscntr].height = None
             
-            #print (maincolm ,":", maincolm in maincolmhdrs)
             if maincolm in maincolmhdrs:
-                #print("fromcolm = ",maincolmhdrs[maincolm])
                 fromcell=exclmainshet.cell(row = rowscntr, column = maincolmhdrs[maincolm])
-                #print("fromcell.value = ",fromcell.value)
                 if fromcell.has_style:
-                    destcell.font          = copy(fromcell.font)
-                    destcell.border        = copy(fromcell.border)
-                    destcell.fill          = copy(fromcell.fill)
-                    destcell.number_format = copy(fromcell.number_format)
-                    destcell.protection    = copy(fromcell.protection)
-                    destcell.alignment     = copy(fromcell.alignment)
-                    
+                    copycellfrmt(fromcell, destcell)
                 destcell.value = fromcell.value 
             else:
+                # Copy style from column one for hard coded values
+                fromcell=exclmainshet.cell(row = rowsstrt+1, column = 1)
+                if fromcell.has_style:
+                    copycellfrmt(fromcell, destcell)
                 destcell.value = thiscolm
             rowscntr += 1
         destcolm += 1
 
+    # _NZ_ CHECK
+    # Check which rows have a zero or empty value in a column marked with _NZ_
+    rowscntr = rowsstrt + 1
+    dlterows = []
+    while rowscntr <= exclrowsused + rowsstrt:
+        for colmnmbr in nzrocols:
+            destcell = destshet.cell(row = rowscntr, column = colmnmbr)
+            if type(destcell.value) in [int, float]:
+                if destcell.value == 0:
+                    if rowscntr not in dlterows:
+                        dlterows.append(rowscntr)
+            elif destcell.value in [None, ""]:
+                if rowscntr not in dlterows:
+                    dlterows.append(rowscntr)
+        rowscntr += 1
+
+    # Now remove them from the sheet
+    dlterows.sort(reverse = True)
+    print (dlterows)
+    if dlterows:
+        for dlterown in dlterows:
+            destshet.delete_rows(dlterown)
+
+    # Add up the totals if there are any
+    if totlflag:
+        rowsused = destshet.max_row
+        for colmnmbr in totlcols:
+            rowscntr = rowsstrt + 1
+            totlvalu = 0
+            while rowscntr <= rowsused:
+                totlcell = destshet.cell(row = rowscntr, column = colmnmbr)
+                if type(totlcell.value) in [int, float]:
+                    totlvalu += totlcell.value
+                rowscntr += 1
+            rowscntr += 1
+            totlcell = destshet.cell(row = rowsused + 1, column = colmnmbr)
+            abovcell = destshet.cell(row = rowsused    , column = colmnmbr)
+            copycellfrmt(abovcell, totlcell)
+            makefontbold(totlcell)
+            totlcell.value = totlvalu
+        
+        totlcell = destshet.cell(row = rowsused + 1, column = 1)
+        copycellfrmt(abovcell, totlcell)
+        makefontbold(totlcell)
+        totlcell.value = "Total:"
+        
+    # Make all the columns auto sizing
     for cols in destshet.columns:
         col = get_column_letter(cols[0].column)
         destshet.column_dimensions[col].auto_size = True
 
+    # Save it if it is a file type
     if defntype == "FILE":
         destbook.save(filename=defnname+".xlsx")
 
+# Save the main sheet anyway
 exclmainbook.save(filename="_"+exclfilename)
 sys.exit()
